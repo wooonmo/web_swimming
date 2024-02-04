678acf7e9b1a143147aa919e24494ff0c293109c
이원모 <outlanderkarl@gmail.com>
2024-02-04 19:40:23 +0900

    240204 add posting page

--- web_study/main/views.py
+++ web_study/main/views.py
@@ -13,3 +13,9 @@ def blog(request):
     postlist = Post.objects.all()
     # blog 페이지를 open할 때 postlist도 함께 open
     return render(request, 'main/blog.html', {'postlist':postlist})
+
+def posting(request, pk):
+    # 게시글(post) 중 pk(primary_key)를 이용해 하나의 게시글(post)을 검색
+    post = Post.objects.get(pk=pk)
+    # posting.html 페이지를 열 때, 찾아낸 게시글(post)을 post라는 이름으로 가져옴
+    return render(request, 'main/posting.html', {'post':post})
